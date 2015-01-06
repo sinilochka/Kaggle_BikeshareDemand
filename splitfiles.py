22956f8eba1dcb5faaa0b5c804801c25c6e7c8a6
Anna Sinilo <sinilochka@users.noreply.github.com>
2015-01-06 14:33:58 -0800

    Update splitfiles.py

--- splitfiles.py
+++ splitfiles.py
@@ -1,13 +1,10 @@
 bikeshare = open('train.csv', 'r')
 subtrain = open('subtrain.csv', 'w')
 validate = open('validate.csv', 'w')
-bikeshare.read()
 for line in bikeshare.readline():
     line = line.strip()
     columns = line.split(',')
-    fields = [line.split(' ') for line in bikeshare]
     datetime = columns[0]
-    print fields
     if int(datetime[8:10]) <= 15:
             print >> subtrain, line.strip()
     else:
