a8990a038e0930f1e14cb6877aa2b1e043d6bd06
Anna Sinilo <sinilochka@users.noreply.github.com>
2015-01-06 14:16:50 -0800

    Create splitfiles.py

---
+++ splitfiles.py
@@ -0,0 +1,15 @@
+bikeshare = open('train.csv', 'r')
+subtrain = open('subtrain.csv', 'w')
+validate = open('validate.csv', 'w')
+bikeshare.read()
+for line in bikeshare.read()
+    line = line.strip()
+    columns = line.split(',')
+    fields = [line.split(' ') for line in bikeshare]
+    print fields
+        if int(datetime[8:10]) <=15:
+            select into subtrain
+        else:
+            select into validate
+subtrain.close()
+validate.close()
